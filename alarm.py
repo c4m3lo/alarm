2b2c4cb6bf1b6d4e04ea72f2180b6b562ef4bb43
Dave Walker <dave@dave.gs>
2012-09-23 20:46:29 +0100

    Better layout of cde2

--- alarm.py
+++ alarm.py
@@ -1,40 +1,17 @@
-import imaplib2
 import argparse
-import time
+from imapfolder import IMAPFolder
 
 
-class IMAPFolder(imaplib2.IMAP4_SSL):
-    def __init__(self, server, username, password, folder):
-        super(IMAPFolder, self).__init__(server)
-        self.login(username, password)
-        self.select(folder)
-        self.new_ids = []
-
-    def __enter__(self):
-        return self
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        while not len(self.new_ids):
-            time.sleep(10)
-            print 'checking'
-            type, data = self.search(None, 'UNSEEN')
-            if type != 'OK':
-                raise StopIteration()
-            self.new_ids = data[0].split()
-        return self.new_ids.pop()
-
-    def __exit__(self, type, value, traceback):
-        self.close()
-        self.logout()
-
-
-def main(username, password):
+def listen_to_jenkins(server='imap.gmail.com',
+                      username=None, password=None,
+                      folder='Jenkins',
+                      job=None,
+                      success=lambda: None,
+                      unstable=lambda: None):
     with IMAPFolder('imap.gmail.com', username, password, 'Jenkins') as folder:
         for new_msg in folder:
-            print new_msg
+            if new_msg['X-Jenkins-Job'] == job:
+                new_msg['X-Jenkins-Result']
 
 
 if __name__ == '__main__':
@@ -42,4 +19,4 @@ if __name__ == '__main__':
     parser.add_argument('username')
     parser.add_argument('password')
     args = parser.parse_args()
-    main(args.username, args.password)
+    listen_to_jenkins(args.username, args.password)
